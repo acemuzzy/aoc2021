ed13d723fffaa182535dacb387e0fa2c3da2187b
Murray Rogers <murray.rogers@metaswitch.com>
2021-12-15 12:48:30 +0000

    Day 15 finally working

--- src/day15.rs
+++ src/day15.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 pub struct Day15;
 
 #[derive(Debug)]
@@ -20,44 +18,16 @@ impl std::fmt::Display for Risks {
 }
 
 impl Risks {
-    fn get(&self, x: i32, y: i32) -> i32 {
-        // if x < 0
-        //     || y < 0
-        //     || x >= self.individual[0].len() as i32
-        //     || y >= self.individual.len() as i32
-        // {
-        //     None
-        // } else {
-        //     Some(self.individual[y as usize][x as usize])
-        // }
-        self.individual[y as usize][x as usize]
-    }
-
-    fn get_cumm(&self, x: i32, y: i32) -> Option<i32> {
-        self.cumulative[y as usize][x as usize]
-    }
-
     fn walk(&mut self) {
-        //println!("Visiting ({},{}), val {}", x, y, cumulative);
-
-        // Start off with having visited nowhere
-        let mut open_points = Vec::<(i32, i32)>::new();
-        for yy in 0..self.cumulative.len() {
-            for xx in 0..self.cumulative[0].len() {
-                open_points.push((xx as i32, yy as i32));
-            }
-        }
+        let mut open_points: Vec<(i32, i32)> = vec![(0, 0)];
 
         // While we've go points we've not visited
         // - Find the unvisited point with the lowest distance - we must have its optimal path
         // - Look at its unvisited neighbours, see if their current-best path can be created/bettered
-        let mut count = 0;
         let width = self.individual[0].len();
         let height = self.individual.len();
 
         loop {
-            count += 1;
-            //let (x, y): (i32, i32) = *open_points.iter().nth(0).unwrap();
             let mut lowest = None;
             let (mut x, mut y): (i32, i32) = (0, 0);
             for (xx, yy) in open_points.iter() {
@@ -82,68 +52,60 @@ impl Risks {
 
             open_points.retain(|(xx, yy)| (xx, yy) != (&x, &y));
 
-            //println!("Checking point ({},{}) ({} of {})", x, y, count, self.cumulative.len() * self.cumulative[0].len());
-
-            //open_points.remove(&(x as i32, y as i32));
-
-            //self.cumulative[y as usize][x as usize] = Some(cumulative);
-            let cumulative = lowest.unwrap(); //self.get_cumm(x, y).unwrap();
+            let cumulative = lowest.unwrap();
             self.best[y as usize][x as usize] = self.cumulative[y as usize][x as usize];
 
-            // let delta = self.get(x - 1, y);
-            // if let Some(d) = delta {
-            //     if let Some(c) = self.get_cumm(x - 1, y) {
-            //         if c > cumulative + d {
-            //             self.cumulative[y as usize][(x - 1) as usize] = Some(cumulative + d);
-            //             open_points.insert((x - 1, y));
-            //         }
-            //     } else {
-            //         self.cumulative[y as usize][(x - 1) as usize] = Some(cumulative + d);
-            //         open_points.insert((x - 1, y));
-            //     }
-            // }
-
-            if x < (width as i32 - 1) {
-                if self.best[y as usize][(x + 1) as usize].is_none() {
-                    let d = self.individual[y as usize][(x + 1) as usize];
-                    if let Some(c) = self.cumulative[y as usize][(x + 1) as usize] {
-                        if c > cumulative + d {
-                            self.cumulative[y as usize][(x + 1) as usize] = Some(cumulative + d);
-                            //open_points.insert((x + 1, y));
-                        }
-                    } else {
-                        self.cumulative[y as usize][(x + 1) as usize] = Some(cumulative + d);
-                        // open_points.insert((x + 1, y));
+            if x > 0 && self.best[y as usize][(x - 1) as usize].is_none() {
+                let d = self.individual[y as usize][(x - 1) as usize];
+                if let Some(c) = self.cumulative[y as usize][(x - 1) as usize] {
+                    if c > cumulative + d {
+                        self.cumulative[y as usize][(x - 1) as usize] = Some(cumulative + d);
+                        open_points.push((x - 1, y));
                     }
+                } else {
+                    self.cumulative[y as usize][(x - 1) as usize] = Some(cumulative + d);
+                    open_points.push((x - 1, y));
                 }
             }
 
-            // let delta = self.get(x, y - 1);
-            // if let Some(d) = delta {
-            //     if let Some(c) = self.get_cumm(x, y - 1) {
-            //         if c > cumulative + d {
-            //             self.cumulative[(y - 1) as usize][x as usize] = Some(cumulative + d);
-            //             open_points.insert((x, y - 1));
-            //         }
-            //     } else {
-            //         self.cumulative[(y - 1) as usize][x as usize] = Some(cumulative + d);
-            //         open_points.insert((x, y - 1));
-            //     }
-            // }
-
-            if y < (height as i32 - 1) {
-                if self.best[(y + 1) as usize][x as usize].is_none() {
-                    let d = self.individual[(y + 1) as usize][x as usize];
-
-                    if let Some(c) = self.cumulative[(y + 1) as usize][x as usize] {
-                        if c > cumulative + d {
-                            self.cumulative[(y + 1) as usize][x as usize] = Some(cumulative + d);
-                            //  open_points.insert((x, y + 1));
-                        }
-                    } else {
-                        self.cumulative[(y + 1) as usize][x as usize] = Some(cumulative + d);
-                        //  open_points.insert((x, y + 1));
+            if x < (width as i32 - 1) && self.best[y as usize][(x + 1) as usize].is_none() {
+                let d = self.individual[y as usize][(x + 1) as usize];
+                if let Some(c) = self.cumulative[y as usize][(x + 1) as usize] {
+                    if c > cumulative + d {
+                        self.cumulative[y as usize][(x + 1) as usize] = Some(cumulative + d);
+                        open_points.push((x + 1, y));
                     }
+                } else {
+                    self.cumulative[y as usize][(x + 1) as usize] = Some(cumulative + d);
+                    open_points.push((x + 1, y));
+                }
+            }
+
+            if y > 0 && self.best[(y - 1) as usize][x as usize].is_none() {
+                let d = self.individual[(y - 1) as usize][x as usize];
+
+                if let Some(c) = self.cumulative[(y - 1) as usize][x as usize] {
+                    if c > cumulative + d {
+                        self.cumulative[(y - 1) as usize][x as usize] = Some(cumulative + d);
+                        open_points.push((x, y - 1));
+                    }
+                } else {
+                    self.cumulative[(y - 1) as usize][x as usize] = Some(cumulative + d);
+                    open_points.push((x, y - 1));
+                }
+            }
+
+            if y < (height as i32 - 1) && self.best[(y + 1) as usize][x as usize].is_none() {
+                let d = self.individual[(y + 1) as usize][x as usize];
+
+                if let Some(c) = self.cumulative[(y + 1) as usize][x as usize] {
+                    if c > cumulative + d {
+                        self.cumulative[(y + 1) as usize][x as usize] = Some(cumulative + d);
+                        open_points.push((x, y + 1));
+                    }
+                } else {
+                    self.cumulative[(y + 1) as usize][x as usize] = Some(cumulative + d);
+                    open_points.push((x, y + 1));
                 }
             }
         }
